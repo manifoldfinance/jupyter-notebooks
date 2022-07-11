dfed52d4147636ead26a47a3a1dfca6b4cbee20d
sam bacha <sam@manifoldfinance.com>
2022-07-11 16:19:46 -0700

    fix(bootstrap): handle venv

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
-python3 -m venv venv
+set -euo pipefail
+#PYTHON=venv/bin/python
+#PYTHON_BIN=venv/bin
+
+if ! [ -d venv ]; then
+  mkdir -p venv
+  virtualenv -p "$(which python3)" venv
+  # shellcheck disable=SC1091
+   source venv/bin/activate
+#  python3 -m pip install --ignore-installed -r requirements.txt
+pip3 install -r requirements.txt
+
+python3 -m ipykernel install --user --name python-cadlabs-eth-model --display-name "Python (NOTEBOOKS)"
+jupyter notebook
+exit 0
+fi
+# shellcheck disable=SC1091
 source venv/bin/activate
 
 echo "Installing requirements..."
 pip3 install -r requirements.txt
 
-python3 -m ipykernel install --user --name python-cadlabs-eth-model --display-name "Python (CURRENTS)"
+python3 -m ipykernel install --user --name python-cadlabs-eth-model --display-name "Python (NOTEBOOKS)"
 sleep 1
+
 jupyter notebook
+
+exit 0
